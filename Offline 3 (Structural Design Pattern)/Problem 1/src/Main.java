5751a777e5f18d791920d34c19053bf045d0c1e3
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2023-02-12 22:49:36 +0600

    Ready for submission

--- Offline 3 (Structural Design Pattern)/Problem 1/src/Main.java
+++ Offline 3 (Structural Design Pattern)/Problem 1/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Crewmate crewmate = new Crewmate();
         Imposter imposter = new Imposter();
-        ImposterAdapter imposterAdapter = new ImposterAdapter(imposter);
+        Passenger imposterAdapter = new ImposterAdapter(imposter);
         maintainSpaceship(crewmate);
         maintainSpaceship(imposterAdapter);
     }
